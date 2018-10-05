3a2001ad7068a08c57e6a9dbc339ae8c2ba2f50a
vilen-kim <vilen_kim@mail.ru>
2018-10-05 20:02:46 +0500

    Пустой проект

---
+++ views/layouts/main.php
@@ -0,0 +1,66 @@
+<?php
+    use app\widgets\Alert;
+    use yii\helpers\Html;
+    use yii\bootstrap\Nav;
+    use yii\bootstrap\NavBar;
+    use yii\widgets\Breadcrumbs;
+    use app\assets\AppAsset;
+
+    AppAsset::register($this);
+?>
+
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php $this->registerCsrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+    <?php
+    NavBar::begin([
+        'brandLabel' => Yii::$app->name,
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-default navbar-fixed-top',
+        ],
+    ]);
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => [
+            ['label' => 'Главная', 'url' => '#main'],
+            ['label' => 'Почему мы?', 'url' => '#why'],
+            ['label' => 'Этапы', 'url' => '#stages'],
+            ['label' => 'О нас', 'url' => '#about'],
+            ['label' => 'Практика', 'url' => '#practice'],
+            ['label' => 'Записаться', 'url' => '#signup'],
+            ['label' => 'Об организации', 'url' => '#organization'],
+        ],
+    ]);
+    NavBar::end();
+    ?>
+
+    <div class="container">
+        <?= $content ?>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
