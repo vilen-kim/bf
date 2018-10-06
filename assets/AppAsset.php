20ba2bb70d42d2b7881e73571cac36e69e1119a5
vilen-kim <vilen_kim@mail.ru>
2018-10-06 14:57:41 +0500

    Почему мы

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -8,8 +8,10 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/animations.css',
     ];
     public $js = [
+        'js/css3-animate-it.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
