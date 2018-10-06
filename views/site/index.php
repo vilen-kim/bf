eafe5d810a12a1386761f39fc3f3be5c656c0e72
vilen-kim <vilen_kim@mail.ru>
2018-10-06 12:57:33 +0500

    Главная страница

--- views/site/index.php
+++ views/site/index.php
@@ -1,1 +1,11 @@
 <?php
+	use yii\helpers\Html;
+?>
+
+<div id="main">
+	<div class="container">
+		<div class="gray" style="position: absolute; top: 490px; left: 520px; font-size: 46px">
+			Оказание юридических услуг
+		</div>
+	</div>
+</div>
