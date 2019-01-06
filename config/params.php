d7e8802cd0ef79f409c48160c5a9bfb448ada89a
vilen-kim <vilen_kim@mail.ru>
2019-01-06 13:43:31 +0500

    clear params.php

--- config/params.php
+++ config/params.php
@@ -1,8 +1,5 @@
 <?php
 
 return [
-    'requests' => 'bona_fides_86@mail.ru',
-    'secret_key' => '6Lf_HXUUAAAAAOoOiW71FyTlLL7CwSmv-Zm7ZpNt',
-    'site_key' => '6Lf_HXUUAAAAAP7P2ZHU3pY8-QHk5aMCSXb6Z6l8',
-    'password' => "DX4Uzs#';(xY&kyW"
+    
 ];
